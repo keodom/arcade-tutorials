8a2907eac7e37ca49300b1323658917775bc6a02
Kelly <165695503+keodom@users.noreply.github.com>
2025-03-07 09:15:23 -0500

    0.0.8

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.61/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.7","target":"arcade","targetVersion":"1.12.61","repo":"keodom/arcade-tutorials"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.61/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.8","target":"arcade","targetVersion":"1.12.61","repo":"keodom/arcade-tutorials"}
 // total=1242396 new=5.85% cached=88.49% other=5.66%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P18176(s) {
+function _main___P18363(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -831,13 +831,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P18176.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P18176.continuations = [  ]
+_main___P18363.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P18363.continuations = [  ]
 
-function _main___P18176_mk(s) {
+function _main___P18363_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P18176, depth: s.depth + 1,
+        parent: s, fn: _main___P18363, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42328,5 +42328,5 @@ const sprites_BaseSpriteSayRenderer__C2260_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P18176
+return _main___P18363
 })
